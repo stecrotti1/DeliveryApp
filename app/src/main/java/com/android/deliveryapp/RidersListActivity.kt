08ccfdb7e9b533932d7e11206d25f53bf80e21af
Stefano Crotti <20029190@studenti.uniupo.it>
2021-02-21 11:06:06 +0100

    Add RidersListActivity.kt

---
+++ app/src/main/java/com/android/deliveryapp/RidersListActivity.kt
@@ -0,0 +1,16 @@
+package com.android.deliveryapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.deliveryapp.databinding.ActivityRidersListBinding
+
+class RidersListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRidersListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRidersListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
